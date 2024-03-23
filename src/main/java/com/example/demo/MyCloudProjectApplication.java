01e3eb5b8702f3265aaa250ea6eb3ed4b4b2c766
ndark <ndark@192.168.219.101>
2024-03-23 22:59:13 +0900

    init commit

---
+++ src/main/java/com/example/demo/MyCloudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCloudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCloudProjectApplication.class, args);
+	}
+
+}
