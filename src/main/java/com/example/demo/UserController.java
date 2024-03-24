ec5dd894679c9a7f3d1b87a95d826a69185e685e
ndark <ndark@192.168.219.101>
2024-03-24 23:41:38 +0900

    1

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,17 +2,16 @@ package com.example.demo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
-@RequiredArgsConstructor
 public class UserController {
 	
-	private final UserMapper userMapper;
+	@Autowired
+	private UserMapper userMapper;
 
 	@GetMapping("/users")
 	public ResponseEntity<List<UserDTO>> selectUserList(){
